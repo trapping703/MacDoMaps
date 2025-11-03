a4c1c7abfa440d65839aae727bed08f0aa6399d0
Nom Prenom <you@exeample.com>
2025-11-03 13:40:22 +0100

    Contenu HTML basic pour les composants

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SearchBar} from './search-bar/search-bar';
+import {Header} from './header/header';
+import {MapInteractive} from './map-interactive/map-interactive';
+import {CityList} from './city-list/city-list';
+import {RestoCard} from './resto-card/resto-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBar, Header, MapInteractive, CityList, RestoCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
