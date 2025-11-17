813535b83a498b107ccfa30b393c2cf82cc5c310
Nom Prenom <you@exeample.com>
2025-11-17 13:05:29 +0100

    map fullscreen plus ou moins + creation component selected-restaurant

---
+++ src/app/components/selected-restaurant/selected-restaurant.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectedRestaurant } from './selected-restaurant';
+
+describe('SelectedRestaurant', () => {
+  let component: SelectedRestaurant;
+  let fixture: ComponentFixture<SelectedRestaurant>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SelectedRestaurant]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SelectedRestaurant);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/selected-restaurant/selected-restaurant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-restaurant',
+  imports: [],
+  templateUrl: './selected-restaurant.html',
+  styleUrl: './selected-restaurant.scss',
+})
+export class SelectedRestaurant {
+
+}
