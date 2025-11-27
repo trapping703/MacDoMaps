55d93b905bffb1244a9cee462522cc5cf6162bce
Nom Prenom <you@exeample.com>
2025-11-27 11:14:58 +0100

    suppression des component inutile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import {Routes} from '@angular/router';
-import {TestRoute} from './components/test-route/test-route';
-import {MapInteractive} from './components/map-interactive/map-interactive';
 import {MapPage} from './components/map-page/map-page';
 
 export const routes: Routes = [
-  {path: '', component: TestRoute},
-  {path: 'test', component: MapPage}];
+  {path: '', component: MapPage},];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {SearchBar} from './components/search-bar/search-bar';
-import {Header} from './components/header/header';
-import {MapInteractive} from './components/map-interactive/map-interactive';
-import {CityList} from './components/city-list/city-list';
-import {RestoCard} from './components/resto-card/resto-card';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SearchBar, Header, MapInteractive, CityList, RestoCard],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
